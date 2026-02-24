c147c66c978b25e75bc64721d3157391c93c9982
adokae84-art <adokae84@gmail.com>
2026-02-24 22:08:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 app = Flask(__name__)
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/")
 def index():
